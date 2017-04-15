d2984d824a789796855e22c4101b652f20ad8650
tfrazzitta <tfrazzitta@yahoo.com>
2017-04-15 11:16:27 -0400

    hh

---
+++ object-generator.js
@@ -0,0 +1,32 @@
+var exports= module.exports= {};
+
+exports.Table = function(id,product_name,department,price,quantity){
+ 	this.name=name,
+ 	this.phone=phone,
+ 	this.email=email,
+ 	this.id=id,
+ 	this.time= moments;
+
+	this.printInfo = function(){
+ 	console.log("id: "+this.name +"\n" + "product name: "+this.phone+"\n" +"department: " + this.email +"\n" + "price: "+ this.id);
+
+ 	this.reservtion= function{
+ 		if(reservtionArray.length<5){
+ 			//this.Table.push(reservtionArray)
+ 			Table.push(reservtionArray);
+ 		}
+ 		else{
+ 			//this.Table.push(reservtionArray)
+ 			Table.push(waitListArray);
+ 		}
+
+ 	}
+
+ }
+
+ };
+
+
+
+
+ //new Table(this.table,this.phone,this.email,this.email,)
